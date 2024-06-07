4ae2ee7571f6cd081b912c017b5825043b821257
julio <juliorzomc@gmail.com>
2024-06-07 11:15:55 -0400

    refectory.route.soma

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-anotacoes relevantes aqui
+# Meu Projeto
+
+Este é o registro dos passos que segui para configurar o projeto:
+
+1. Inicializei o projeto com o npm:
+    ```
+    npm init -y
+    ```
+2. Iniciei o projeto:
+    ```
+    npm start
+    ```
+3. Instalei o Express, um framework para aplicação web:
+    ```
+    npm install express
+    ```
+4. Instalei o Nodemon para reiniciar automaticamente o servidor sempre que houver uma alteração no código:
+    ```
+    npm install nodemon
+    ```
+5. Executei o projeto em modo de desenvolvimento:
+    ```
+    npm run dev
+    ```
--- index.js
+++ index.js
@@ -7,9 +7,26 @@ const app = express();
 // Define uma rota GET para a URL raiz ("/") da aplicação
 // Quando alguém acessa a URL raiz, a função de callback é chamada
 // req é o objeto de solicitação e res é o objeto de resposta
+
+//ROTA
+// Method HTTP - CRUD (Create, Read, Update, Delete)
+    // GET - Pegar uma informações
+    // POST - Enviar / Criar uma informações
+    // PUT - Atualizar informações
+    // PATH - Atualizar uma informação específica
+    // DELETE - Deletar uma informação
+// Name
+    // Um nome para a rota. Ex: /users
+// Function (Callback)
+    // Função que será executada quando a rota for acessada
+    // Callback: Função que é passada como argumento para outra função
 app.get('/', (req, res) => {
     // A função de callback envia a string 'Hello World' como resposta
-    res.send('Hello World');
+    // res.send('Hello World');
+    const soma = 2 + 2;
+    res.json(soma); 
+    //ou
+    // res.send({soma:soma});
 });
 
 // A aplicação começa a escutar na porta 3000
